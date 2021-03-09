d9ba033566c948269019ae78b9b06aa9174c89de
Pat <patrick.brock87@googlemail.com>
2021-03-09 19:32:51 +0100

    Section 14 - new DTO, Like Table and Repository

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -29,11 +29,8 @@ namespace API.Entities
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }        
-
-        // public int GetAge(){
-
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public ICollection<Photo> Photos { get; set; }                
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUser { get; set; }
     }
 }
